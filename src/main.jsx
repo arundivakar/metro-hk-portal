f96c839b43058e4e1e820a6e92c38ff42e247c60
arundivakar <arunkanayi@gmail.com>
2026-08-07 15:32:24 +0530

    ux_disable_number_input_scroll_and_spinners

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+// Prevent scrolling on number inputs from changing the value
+document.addEventListener('wheel', () => {
+  if (document.activeElement.type === 'number') {
+    document.activeElement.blur();
+  }
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
